0973058531d0517cf9b5182ae0bac160c7b04c57
Vijayakumar <md.axebelk@gmail.com>
2021-01-02 15:51:40 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Ippopay } from 'react-ippopay';
 
 function App() {
+  const [paymentOpen, setPaymentOpen] = useState(false)
+  const [paymentLoading, setPaymentLoading] = useState(false)
+  const [orderId, setOrderId] = useState("")
+  const api = axios.create({
+    baseURL: `https://dev.apiv2.truedreem.com`,
+    headers: {
+      'Authorization': "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjEwMDA0NzM5LCJqdGkiOiIxYjIzY2I1MzExY2U0OWU5YjE4OWU5NjJhYmEyMDJmMSIsInJpZCI6IjI1NzVhOTkyLTRmZGMtNGEwYS1iMmVkLTg2YjUyN2NiZjJmOSJ9.zv1R3grGo4jdZtrGOXqTDmAgeYvmAIbpf9ITreIcnsg",
+      'Content-Type': 'application/json'
+    }
+  })
+  useEffect(() => {
+    window.addEventListener('message', ippopayHandler)
+
+    return () => window.removeEventListener('message', ippopayHandler)
+  })
+
+  const ippopayHandler = (e: any) => {
+    setPaymentLoading(false)
+    if (e.data.status === 'success') {
+      console.log(e.data)
+    }
+    if (e.data.status === 'failure') {
+      console.log(e.data)
+    }
+  }
+
+  /* 
+  Authorization header is server token
+  */
+  const makePayment = () => {
+    setPaymentLoading(true)
+    api.post("/auction/create-order").then(res => {
+      setOrderId(res.data.data.order.order_id)
+      setPaymentOpen(true)
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => makePayment()}>{paymentLoading ? "Loading..." : "MAKE PAYMENT"}</button>
+      <Ippopay
+        ippopayOpen={paymentOpen}
+        ippopayClose={true}
+        order_id={orderId}
+        public_key={`pk_live_TvLSKXeLxFVF`}
+      />
     </div>
   );
 }
